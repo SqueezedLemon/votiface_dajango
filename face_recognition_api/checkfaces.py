b4100f1455782908535fbc2e3a546dc8f10d3a90
Biraj Adhikari <birajkmc4@gmail.com>
2022-04-25 14:03:04 +0545

    #refactor #short-and-sweet #sem-project

--- face_recognition_api/checkfaces.py
+++ face_recognition_api/checkfaces.py
@@ -2,12 +2,6 @@ from fileinput import filename
 import cv2
 import face_recognition
 
-def compare(faceDistance):
-    if faceDistance > 0.5:  #reduce this value if you want to increase accuracy
-        return False
-    else:
-        return True
-
 def findEncoding(image): #returns 128-d encoding of the image
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     encode = face_recognition.face_encodings(image)[0]
@@ -15,5 +9,4 @@ def findEncoding(image): #returns 128-d encoding of the image
 
 def comparison(firstphoto,secondphoto): #compares the two images and returns the bool result
     faceDistance = face_recognition.face_distance([firstphoto],secondphoto)
-    results = compare(faceDistance)
-    return results
+    return False if faceDistance > 0.5 else True
